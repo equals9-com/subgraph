77026a7b692ecd9825799afdb065a40ad99367bb
Pedro Bufulin <pedrohenriquebufulindealmeida@gmail.com>
2022-09-27 17:11:45 -0300

    feat: generated schemas schema duplicate and handlers

--- equalssport-staking-mainnet/generated/schema.ts
+++ equalssport-staking-mainnet/generated/schema.ts
@@ -51,6 +51,192 @@ export class ExampleEntity extends Entity {
     this.set("count", Value.fromBigInt(value));
   }
 
+  get previousOwner(): Bytes {
+    let value = this.get("previousOwner");
+    return value!.toBytes();
+  }
+
+  set previousOwner(value: Bytes) {
+    this.set("previousOwner", Value.fromBytes(value));
+  }
+
+  get newOwner(): Bytes {
+    let value = this.get("newOwner");
+    return value!.toBytes();
+  }
+
+  set newOwner(value: Bytes) {
+    this.set("newOwner", Value.fromBytes(value));
+  }
+}
+
+export class Staker extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id != null, "Cannot save Staker entity without an ID");
+    if (id) {
+      assert(
+        id.kind == ValueKind.STRING,
+        `Entities of type Staker must have an ID of type String but the id '${id.displayData()}' is of type ${id.displayKind()}`
+      );
+      store.set("Staker", id.toString(), this);
+    }
+  }
+
+  static load(id: string): Staker | null {
+    return changetype<Staker | null>(store.get("Staker", id));
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value!.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get address(): Bytes {
+    let value = this.get("address");
+    return value!.toBytes();
+  }
+
+  set address(value: Bytes) {
+    this.set("address", Value.fromBytes(value));
+  }
+
+  get totalStaked(): BigInt {
+    let value = this.get("totalStaked");
+    return value!.toBigInt();
+  }
+
+  set totalStaked(value: BigInt) {
+    this.set("totalStaked", Value.fromBigInt(value));
+  }
+
+  get stakesAmount(): i32 {
+    let value = this.get("stakesAmount");
+    return value!.toI32();
+  }
+
+  set stakesAmount(value: i32) {
+    this.set("stakesAmount", Value.fromI32(value));
+  }
+}
+
+export class Player extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id != null, "Cannot save Player entity without an ID");
+    if (id) {
+      assert(
+        id.kind == ValueKind.STRING,
+        `Entities of type Player must have an ID of type String but the id '${id.displayData()}' is of type ${id.displayKind()}`
+      );
+      store.set("Player", id.toString(), this);
+    }
+  }
+
+  static load(id: string): Player | null {
+    return changetype<Player | null>(store.get("Player", id));
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value!.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get address(): Bytes {
+    let value = this.get("address");
+    return value!.toBytes();
+  }
+
+  set address(value: Bytes) {
+    this.set("address", Value.fromBytes(value));
+  }
+
+  get totalStaked(): BigInt {
+    let value = this.get("totalStaked");
+    return value!.toBigInt();
+  }
+
+  set totalStaked(value: BigInt) {
+    this.set("totalStaked", Value.fromBigInt(value));
+  }
+
+  get stakesAmount(): i32 {
+    let value = this.get("stakesAmount");
+    return value!.toI32();
+  }
+
+  set stakesAmount(value: i32) {
+    this.set("stakesAmount", Value.fromI32(value));
+  }
+}
+
+export class Stake extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id != null, "Cannot save Stake entity without an ID");
+    if (id) {
+      assert(
+        id.kind == ValueKind.STRING,
+        `Entities of type Stake must have an ID of type String but the id '${id.displayData()}' is of type ${id.displayKind()}`
+      );
+      store.set("Stake", id.toString(), this);
+    }
+  }
+
+  static load(id: string): Stake | null {
+    return changetype<Stake | null>(store.get("Stake", id));
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value!.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get staker(): string {
+    let value = this.get("staker");
+    return value!.toString();
+  }
+
+  set staker(value: string) {
+    this.set("staker", Value.fromString(value));
+  }
+
+  get player(): string {
+    let value = this.get("player");
+    return value!.toString();
+  }
+
+  set player(value: string) {
+    this.set("player", Value.fromString(value));
+  }
+
   get amount(): BigInt {
     let value = this.get("amount");
     return value!.toBigInt();
--- equalssport-staking-mainnet/src/staking.ts
+++ equalssport-staking-mainnet/src/staking.ts
@@ -8,64 +8,151 @@ import {
   Unpaused,
   Unstaked
 } from "../generated/Staking/Staking"
-import { ExampleEntity } from "../generated/schema"
+import { ExampleEntity, Player, Stake, Staker } from "../generated/schema"
 
 export function handleClaimed(event: Claimed): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  // // Entities can be loaded from the store using a string ID; this ID
+  // // needs to be unique across all entities of the same type
+  // let entity = ExampleEntity.load(event.transaction.from.toHex())
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+  // // Entities only exist after they have been saved to the store;
+  // // `null` checks allow to create entities on demand
+  // if (!entity) {
+  //   entity = new ExampleEntity(event.transaction.from.toHex())
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
+  //   // Entity fields can be set using simple assignments
+  //   entity.count = BigInt.fromI32(0)
+  // }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  // // BigInt and BigDecimal math are supported
+  // entity.count = entity.count + BigInt.fromI32(1)
 
-  // Entity fields can be set based on event parameters
-  entity.amount = event.params.amount
-  entity.timestamp = event.params.timestamp
+  // // Entity fields can be set based on event parameters
+  // entity.amount = event.params.amount
+  // entity.timestamp = event.params.timestamp
 
-  // Entities can be written to the store with `.save()`
-  entity.save()
+  // // Entities can be written to the store with `.save()`
+  // entity.save()
 
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
+  // // Note: If a handler doesn't require existing field values, it is faster
+  // // _not_ to load the entity from the store. Instead, create it fresh with
+  // // `new Entity(...)`, set the fields that should be updated and save the
+  // // entity back to the store. Fields that were not set or unset remain
+  // // unchanged, allowing for partial updates to be applied.
 
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.claimAmount(...)
-  // - contract.fetchPlayerStakes(...)
-  // - contract.fetchStakersAmount(...)
-  // - contract.lastTimeUserUnstake(...)
-  // - contract.name(...)
-  // - contract.owner(...)
-  // - contract.paused(...)
-  // - contract.stakerAmounts(...)
-  // - contract.stakerTimestamps(...)
+  // // It is also possible to access smart contracts from mappings. For
+  // // example, the contract that has emitted the event can be connected to
+  // // with:
+  // //
+  // // let contract = Contract.bind(event.address)
+  // //
+  // // The following functions can then be called on this contract to access
+  // // state variables and other data:
+  // //
+  // // - contract.claimAmount(...)
+  // // - contract.fetchPlayerStakes(...)
+  // // - contract.fetchStakersAmount(...)
+  // // - contract.lastTimeUserUnstake(...)
+  // // - contract.name(...)
+  // // - contract.owner(...)
+  // // - contract.paused(...)
+  // // - contract.stakerAmounts(...)
+  // // - contract.stakerTimestamps(...)
 }
 
 export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
 
-export function handlePaused(event: Paused): void {}
-
-export function handleStaked(event: Staked): void {}
-
 export function handleUnpaused(event: Unpaused): void {}
 
-export function handleUnstaked(event: Unstaked): void {}
+export function handlePaused(event: Paused): void {}
+
+export function handleStaked(event: Staked): void {  
+  const amount = event.params.amount;
+  const playerAddress = event.params.player;
+  const stakerAddress = event.params.staker;
+  const timestamp = event.params.timestamp;
+
+  let player = Player.load(playerAddress.toString())
+
+  if (!player) {
+    player = new Player(playerAddress.toString())
+    player.totalStaked = amount;
+    player.address = playerAddress
+    player.stakesAmount = 1
+  } else {
+    player.totalStaked = amount.plus(player.totalStaked)
+  }
+
+  let staker = Staker.load(stakerAddress.toString())
+  if (!staker) {
+    staker = new Staker(stakerAddress.toString())
+    staker.totalStaked = amount;
+    staker.address = stakerAddress
+    staker.stakesAmount = 1;
+  } else {
+    staker.totalStaked = amount.plus(staker.totalStaked)
+  }
+
+  let stake = Stake.load(stakerAddress.toHexString() + "-" + playerAddress.toHexString())
+
+  if(!stake){
+    stake = new Stake(stakerAddress.toHexString() + "-" + playerAddress.toHexString())
+    stake.amount = amount;
+    stake.staker = staker.id;
+    stake.player = player.id;
+    stake.timestamp = timestamp;
+    staker.stakesAmount = staker.stakesAmount + 1
+    player.stakesAmount = player.stakesAmount + 1
+  } else {
+    if (stake.amount.equals(BigInt.zero())) {
+      staker.stakesAmount = staker.stakesAmount + 1
+      player.stakesAmount = player.stakesAmount + 1
+    }
+    stake.amount = stake.amount.plus(amount)
+  }
+
+
+  staker.save()
+  player.save()
+  stake.save()
+  return
+}
+
+
+export function handleUnstaked(event: Unstaked): void {
+    const amount = event.params.amount;
+    const playerAddress = event.params.player;
+    const stakerAddress = event.params.staker;
+
+    let player = Player.load(playerAddress.toString())
+
+    if(player){
+      player.totalStaked = player.totalStaked.minus(amount);
+    } 
+
+    let staker = Staker.load(stakerAddress.toString())
+    if (staker){
+      staker.totalStaked = staker.totalStaked.minus(amount)
+    }
+
+    let stake = Stake.load(stakerAddress.toHexString() + "-" + playerAddress.toHexString())  
+    if (stake){
+      stake.amount = stake.amount.minus(amount)
+      if(stake.amount.equals(BigInt.zero())) {
+        stake.timestamp = BigInt.zero() 
+        if(staker && player){
+          staker.stakesAmount = staker.stakesAmount - 1
+          player.stakesAmount = player.stakesAmount - 1
+        }
+      }
+    }
+
+  if(player && staker && stake){
+      player.save()
+      stake.save()
+      staker.save()
+  }
+    return
+}
+
+
